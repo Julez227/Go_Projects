679618277ea7a21dfc8f4b3101b375c06b21136c
julius <311222868+Julez227@users.noreply.github.com>
2026-08-08 00:26:17 -0600

    work

--- range/range.go
+++ range/range.go
@@ -1,8 +1,14 @@
 package main
 
+import "fmt"
 
-func main () {
+func main() {
 
+	message := "Hello World!"
 
+	for i, v := range message {
+		// fmt.Println(i, v)
+		fmt.Printf("Index: %d, Rune: %c\n", i, v)
+	}
 
 }
